ddcaf7bf8d74bcce067e768bec6f9a95daf158aa
mateusoliiveira <teusolive@gmail.com>
2022-08-18 21:29:42 -0300

    fix: env variables

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-export const serverURLApi = process.env.APP_SERVER_URL_API
-export const clientURLApi = process.env.APP_CLIENT_URL_API
-export const clientURL = process.env.NEXTAUTH_URL
+export const serverURLApi = process.env.APP_SERVER_URL_API || "https://outrachave.herokuapp.com/api/v1"
+export const clientURLApi = process.env.APP_CLIENT_URL_API || "https://outrachave.vercel.app/api"
+export const clientURL = process.env.NEXTAUTH_URL || "https://outrachave.vercel.app"
 
