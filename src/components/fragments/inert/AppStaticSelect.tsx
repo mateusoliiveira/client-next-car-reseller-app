821806e3b69ef8019e642c73b8483ac1261c1180
mateusoliiveira <teusolive@gmail.com>
2022-08-18 21:20:31 -0300

    fix: select component

--- src/components/fragments/inert/AppStaticSelect.tsx
+++ src/components/fragments/inert/AppStaticSelect.tsx
@@ -1,36 +1,35 @@
-import { Select, SelectProps } from "flowbite-react"
-import { ReactElement, SelectHTMLAttributes } from "react"
+import { Select, SelectProps } from "flowbite-react";
+import { ReactElement, SelectHTMLAttributes } from "react";
 
 const AppStaticSelect = ({
-  placeholder,
-  defaultValue,
-  id,
-  name,
-  title,
-  value,
-  helperText,
-  children,
-  onChange,
+	placeholder,
+	defaultValue,
+	id,
+	name,
+	title,
+	value,
+	helperText,
+	children,
+	onChange,
 }: SelectProps & SelectHTMLAttributes<HTMLSelectElement>): ReactElement => {
-  return (
-    <div className="relative mb-4">
-      <label htmlFor={id} className="text-red-500 font-extrabold">
-        {title}
-      </label>
-      <Select
-        className="my-1"
-        id={id}
-        name={name}
-        value={value}
-        defaultValue={defaultValue}
-        placeholder={placeholder}
-        helperText={helperText}
-        onChange={onChange}
-      >
-        {children}
-      </Select>
-    </div>
-  )
-}
+	return (
+		<div className="relative mb-4 my-1">
+			<label htmlFor={id} className="text-red-500 font-extrabold">
+				{title}
+			</label>
+			<Select
+				id={id}
+				name={name}
+				value={value}
+				defaultValue={defaultValue}
+				placeholder={placeholder}
+				helperText={helperText}
+				onChange={onChange}
+			>
+				{children}
+			</Select>
+		</div>
+	);
+};
 
-export default AppStaticSelect
+export default AppStaticSelect;
