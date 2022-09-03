8f844d929a0207eb07e26d053876be411c1189e2
mateusoliiveira <teusolive@gmail.com>
2022-09-02 22:14:45 -0300

    fix: validation inputs

--- src/components/fragments/inert/AppStaticInputValidation.tsx
+++ src/components/fragments/inert/AppStaticInputValidation.tsx
@@ -3,39 +3,39 @@ import React from "react";
 const AppStaticInputValidation = ({
 	validation,
 }: {
-	validation: string | string[];
-}) => {
+	validation?: string | string[];
+}): any => {
 	return (
-		validation &&
-		(Array.isArray(validation) && validation.length > 1 ? (
-			validation.map((msg: string) => {
-				return (
-					<>
-						<small
-							style={{
-								color: "red",
-								fontWeight: "bold",
-							}}
-						>
-							{msg}
-						</small>
-						<hr />
-					</>
-				);
-			})
-		) : (
-			<>
-				<small
-					style={{
-						color: "red",
-						fontWeight: "bold",
-					}}
-				>
-					{String(validation)}
-				</small>
-				<hr />
-			</>
-		))
+		(validation &&
+			(Array.isArray(validation) && validation.length > 1 ? (
+				validation.map((msg: string): JSX.Element => {
+					return (
+						<>
+							<small
+								style={{
+									color: "red",
+									fontWeight: "bold",
+								}}
+							>
+								{msg}
+							</small>
+							<hr />
+						</>
+					);
+				})
+			) : (
+				<>
+					<small
+						style={{
+							color: "red",
+							fontWeight: "bold",
+						}}
+					>
+						{String(validation)}
+					</small>
+					<hr />
+				</>
+			))) || <></>
 	);
 };
 
