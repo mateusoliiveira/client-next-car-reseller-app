72a786e3b39d3e31dce5bb99da1bbceb6bcfe272
mateusoliiveira <teusolive@gmail.com>
2022-09-02 22:24:20 -0300

    fix: label

--- src/components/fragments/inert/AppStaticInputLabel.tsx
+++ src/components/fragments/inert/AppStaticInputLabel.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-const AppStaticInputLabel = ({ id, title }: { id: string; title: string }) => {
+const AppStaticInputLabel = ({
+	id,
+	title,
+}: {
+	id?: string;
+	title?: string;
+}) => {
 	return (
 		<label htmlFor={id} className="text-red-500 font-extrabold">
 			{title}
